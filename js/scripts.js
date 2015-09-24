ba7ca7a1b9ff5a9f2d4623e4ca4512240726adc3
Ariel Spear <simpleredfox@gmail.com>
2015-09-24 15:00:19 -0700

    Hooked up front end

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,27 @@
 var sing = function(drink, numberOfBottles) {
   if(numberOfBottles <= 0) {
-    var result = "No more bottles of " + drink + " on the wall, no more bottles of " + drink + ". Go to the store and buy some more, so many bottles of " + drink + " on the wall. ";
+    var result = "No more bottles of " + drink + " on the wall, no more bottles of " + drink + ". Go to the store and buy some more, so many bottles of " + drink + " on the wall. <br>";
     return result;
   } else if(numberOfBottles === 1) {
-    var result = numberOfBottles + " bottle of " + drink + " on the wall, " + numberOfBottles + " bottle of " + drink + ". Take one down, pass it around no more bottles of " + drink + " on the wall. ";
-    return result;
+    var result = numberOfBottles + " bottle of " + drink + " on the wall, " + numberOfBottles + " bottle of " + drink + ". Take one down, pass it around no more bottles of " + drink + " on the wall. <br>";
+    return result + sing(drink, (numberOfBottles - 1));;
   } else {
-    result =  numberOfBottles + " bottles of " + drink + " on the wall, " + numberOfBottles + " bottles of " + drink + ". Take one down, pass it around " + (numberOfBottles - 1) + " bottles of " + drink + " on the wall. ";
-    return result;
+    result =  numberOfBottles + " bottles of " + drink + " on the wall, " + numberOfBottles + " bottles of " + drink + ". Take one down, pass it around " + (numberOfBottles - 1) + " bottles of " + drink + " on the wall. <br>";
+    return result + sing(drink, (numberOfBottles - 1));;
   }
-}
+};
+
+$(document).ready(function() {
+  $("form#bottles").submit(function(event) {
+    var numberOfBottles = parseInt($("input#number").val());
+    var drink = ($("input#drink").val());
+
+    var result = sing(drink, numberOfBottles);
+
+    $(".answer").html(result);
+
+
+    $("#result").show();
+    event.preventDefault();
+  });
+});
--- spec/specs.js
+++ spec/specs.js
@@ -1,11 +1,17 @@
 describe('sing', function() {
   it("uses correct last line", function() {
-    expect(sing("tea", 0)).to.equal("No more bottles of tea on the wall, no more bottles of tea. Go to the store and buy some more, so many bottles of tea on the wall. ");
+    expect(sing("tea", 0)).to.equal("No more bottles of tea on the wall, no more bottles of tea. Go to the store and buy some more, so many bottles of tea on the wall. <br>");
   })
   it("correctly sings with one bottle left", function() {
-    expect(sing("juice", 1)).to.equal("1 bottle of juice on the wall, 1 bottle of juice. Take one down, pass it around no more bottles of juice on the wall. ");
+    expect(sing("juice", 1)).to.equal("1 bottle of juice on the wall, 1 bottle of juice. Take one down, pass it around no more bottles of juice on the wall. <br>No more bottles of juice on the wall, no more bottles of juice. Go to the store and buy some more, so many bottles of juice on the wall. <br>");
   })
   it("sings with more than one bottle left", function() {
-    expect(sing("coffee", 4)).to.equal("4 bottles of coffee on the wall, 4 bottles of coffee. Take one down, pass it around 3 bottles of coffee on the wall. ");
+    expect(sing("coffee", 4)).to.equal("4 bottles of coffee on the wall, 4 bottles of coffee. Take one down, pass it around 3 bottles of coffee on the wall. <br>3 bottles of coffee on the wall, 3 bottles of coffee. Take one down, pass it around 2 bottles of coffee on the wall. <br>2 bottles of coffee on the wall, 2 bottles of coffee. Take one down, pass it around 1 bottles of coffee on the wall. <br>1 bottle of coffee on the wall, 1 bottle of coffee. Take one down, pass it around no more bottles of coffee on the wall. <br>No more bottles of coffee on the wall, no more bottles of coffee. Go to the store and buy some more, so many bottles of coffee on the wall. <br>");
+  })
+  it("treats degative numbers as zero", function() {
+    expect(sing("water", -1)).to.equal("No more bottles of water on the wall, no more bottles of water. Go to the store and buy some more, so many bottles of water on the wall. <br>");
+  })
+  it("won't have extra numbers", function() {
+    expect(sing("beer", 2)).to.not.include("3");
   })
 });
